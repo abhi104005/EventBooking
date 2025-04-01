ad05b4bd5d9b057cf5a929bcf34830bd0490ce97
abhi104005 <abhishekingle995@gmail.com>
2025-04-01 13:18:39 +0530

    change rabbitmq connection

--- backend/RabbitMQ/Producer.js
+++ backend/RabbitMQ/Producer.js
@@ -1,12 +1,16 @@
 const amqp = require("amqplib")
 
 async function sendMessage(message) {
-    const connection = await amqp.connect("amqp://localhost");
-    const channel = await connection.createChannel();
-    const queue = "task";
+    try {
+        const connection = await amqp.connect("amqp://host.docker.internal");
+        const channel = await connection.createChannel();
+        const queue = "task";
 
-    await channel.assertQueue(queue,{durable:true});
-    channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), { persistent: true });
-    console.log(JSON.stringify(message));
+        await channel.assertQueue(queue, { durable: true });
+        channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), { persistent: true });
+        console.log(JSON.stringify(message));
+    } catch (error) {
+        console.error("❌ Failed to connect to RabbitMQ:", error);
+    }
 }
-module.exports = {sendMessage};
+module.exports = { sendMessage };
